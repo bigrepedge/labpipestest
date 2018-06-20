ba4a0702c88ca14106a85626d6143ce25dc14f8a
karl <k.burton@bigrep.com>
2018-06-20 17:26:46 +0200

    edited

--- app/client/vue_app/src/router.js
+++ app/client/vue_app/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Api from './views/Api.vue'
+import Main from './views/Main.vue'
 
 Vue.use(Router)
 
@@ -9,13 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/api',
-      name: 'api',
-      component: Api
+      name: 'main',
+      component: Main
     }
   ]
 })
---
+++ app/client/vue_app/src/views/Main.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="content">
+    <div class="panel">
+      <p>Click on the links below to fetch data from the Flask server</p>
+      <a href="" @click.prevent="fetchResource">Fetch</a><br/>
+      <a href="" @click.prevent="fetchSecureResource">Fetch Secure Resource</a>
+      <h4>Results</h4>
+      <p v-for="r in resources" :key="r.timestamp">
+        Server Timestamp: {{r.timestamp | formatTimestamp }}
+      </p>
+      <p>{{error}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import $backend from '../backend'
+
+export default {
+  name: 'about',
+  data () {
+    return {
+      resources: [],
+      error: ''
+    }
+  },
+  methods: {
+    fetchResource () {
+      $backend.fetchResource()
+        .then(responseData => {
+          this.resources.push(responseData)
+        }).catch(error => {
+          this.error = error.message
+        })
+    },
+    fetchSecureResource () {
+      $backend.fetchSecureResource()
+      .then(responseData => {
+        this.resources.push(responseData)
+      }).catch(error => {
+        this.error = error.message
+      })
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+</style>

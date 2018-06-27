d2f3cdd25c538eafc7e20bef3c3b81fd898bc14e
karl <k.burton@bigrep.com>
2018-06-27 10:37:54 +0200

    some edits to html page

--- app/client/vue_app/src/views/Main.vue
+++ app/client/vue_app/src/views/Main.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="content">
     <div class="panel">
-      <p>Click on the links below to fetch data from the Flask server</p>
-      <p>testing random stuff</p>
-      <a href="" @click.prevent="fetchResource">Fetch Resource</a><br/>
-      <h4>Results</h4>
+      <p>Click on the links below to fetch data from the Flask server (probably still works but we have an error in compiled vendor code)</p>
+      <p>Random changes to identify branch</p>
+      <h4>We had a server call here but it was failing because of a fault in vendor file compilation will look into using another file compiler</h4>
       <p v-for="r in resources" :key="r.timestamp">
         Server Timestamp: {{r.timestamp | formatTimestamp }}
       </p>
